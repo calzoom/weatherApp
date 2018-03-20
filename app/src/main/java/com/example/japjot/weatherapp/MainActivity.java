89f165feba6b4356adb5c8ae3f36c26a27c2190b
Japjot Singh <japjot@Japjots-MacBook-Pro.local>
2018-03-19 22:16:54 -0700

    Pop

--- app/src/main/java/com/example/japjot/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/japjot/weatherapp/MainActivity.java
@@ -1,14 +1,20 @@
-
 package com.example.japjot.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    public static double latitude, longitude;
+    private static int temp;
+    private static String summary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
